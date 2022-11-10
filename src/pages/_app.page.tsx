19caef3defe6354c6cac87c2c7bc1d395492d385
Mikhail T <totalism@icloud.com>
2022-11-10 08:55:23 +0700

    feat: add matomo

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+
+import { init } from '@socialgouv/matomo-next'
 
 import 'styles/variables.css'
 import 'styles/global.css'
 
-const App = ({ Component, pageProps }: AppProps): React.ReactNode => (
-  <React.StrictMode>
-    <Component {...pageProps} />
-  </React.StrictMode>
-)
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL as string
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID as string
+
+const App = ({ Component, pageProps }: AppProps): React.ReactNode => {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID })
+  }, [])
+
+  return (
+    <React.StrictMode>
+      <Component {...pageProps} />
+    </React.StrictMode>
+  )
+}
 
 export default App
