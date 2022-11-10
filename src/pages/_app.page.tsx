30dc8893b0072e96daa59b74470f557d9d833552
Mikhail T <totalism@icloud.com>
2022-11-10 08:49:15 +0700

    feat: remove microanalytics

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 
-import Script from 'next/script'
-
 import 'styles/variables.css'
 import 'styles/global.css'
 
 const App = ({ Component, pageProps }: AppProps): React.ReactNode => (
   <React.StrictMode>
-    <Script
-      src="https://microanalytics.io/js/script.js"
-      data-host="https://microanalytics.io"
-      data-dnt="false"
-      id="ZwSg9rf6GA"
-    />
-
     <Component {...pageProps} />
   </React.StrictMode>
 )
